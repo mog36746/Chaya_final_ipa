f8711b4f49c2587b27b34875cfa606bce53fda4d
Jon <mog36746@gmail.com>
2020-04-17 05:30:22 +0600

    Initial Commit

---
+++ Chaya/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chaya
+//
+//  Created by MI on 4/17/20.
+//  Copyright © 2020 Mog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
